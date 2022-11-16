9c3f9646da830b6d153b3645688d69b3c71e1ae2
fahmed015 <fahmed015@hotmail.com>
2022-11-16 12:49:31 +0200

    Update MeetingRoom.js

--- src/Pages/MeetingRoom.js
+++ src/Pages/MeetingRoom.js
@@ -38,14 +38,14 @@ function MeetingRoom(props) {
     }
   };
 
-  //window.onbeforeunload = (event) => {
-  //const e = event || window.event;
-  //e.preventDefault();
-  // if (e) {
-  //   e.returnValue = ''; // Legacy method for cross browser support
-  // }
-  // return ''; // Legacy method for cross browser support
-  //};
+  window.onbeforeunload = (event) => {
+    const e = event || window.event;
+    e.preventDefault();
+    // if (e) {
+    //   e.returnValue = ''; // Legacy method for cross browser support
+    // }
+    // return ''; // Legacy method for cross browser support
+  };
 
   useEffect(() => {
     navigator.mediaDevices
